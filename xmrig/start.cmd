1ed11e26932114665733714dfc0ab7158af86547
Satria <faridsatria24@gmail.com>
2025-01-24 22:11:53 +0700

    add some sintaks

--- xmrig/start.cmd
+++ xmrig/start.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u DOGE:DSycjXnngRekwJKRQKXJwY88GPw4w9FmQA.DELL -p x
+xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u DOGE:DSycjXnngRekwJKRQKXJwY88GPw4w9FmQA.DELL -p x -t 4 -i 1 -w 10 -p 0 -s 0
 pause
